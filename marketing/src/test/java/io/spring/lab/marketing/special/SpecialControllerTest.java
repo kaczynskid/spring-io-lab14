6ca5aee74abefb82f4a025fe314b7fe1807b69df
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-21 12:31:33 +0200

    Fixed tests

--- marketing/src/test/java/io/spring/lab/marketing/special/SpecialControllerTest.java
+++ marketing/src/test/java/io/spring/lab/marketing/special/SpecialControllerTest.java
@@ -2,6 +2,10 @@ package io.spring.lab.marketing.special;
 
 import static java.util.Collections.singletonList;
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
 
@@ -13,8 +17,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SpecialController.class)
@@ -31,11 +33,13 @@ public class SpecialControllerTest {
         doReturn(singletonList(new Special("A", 1, 3, BigDecimal.valueOf(70))))
             .when(items).findAll();
 
-        mvc.perform(MockMvcRequestBuilders.get("/specials"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].itemId").value("1"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].batchSize").value("3"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].batchPrice").value("70"));
+        mvc.perform(get("/specials"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.length()").value("1"))
+                .andExpect(jsonPath("$.[0].id").doesNotExist())
+                .andExpect(jsonPath("$.[0].itemId").value("1"))
+                .andExpect(jsonPath("$.[0].batchSize").value("3"))
+                .andExpect(jsonPath("$.[0].batchPrice").value("70"));
     }
 }
