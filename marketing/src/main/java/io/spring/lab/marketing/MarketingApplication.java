fd0492603ad10665b3b3ad4a8a86eb3f57e5c7bd
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-21 12:33:21 +0200

    Added specials controller

--- marketing/src/main/java/io/spring/lab/marketing/MarketingApplication.java
+++ marketing/src/main/java/io/spring/lab/marketing/MarketingApplication.java
@@ -1,12 +1,52 @@
 package io.spring.lab.marketing;
 
+import java.math.BigDecimal;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import io.spring.lab.marketing.special.Special;
+import io.spring.lab.marketing.special.SpecialRepository;
+import io.spring.lab.marketing.special.select.BestSpecialSelector;
+import io.spring.lab.marketing.special.select.SpecialSelector;
+import io.spring.lab.math.MathProperties;
+import lombok.AllArgsConstructor;
 
 @SpringBootApplication
 public class MarketingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketingApplication.class, args);
+	}
+
+	@Bean
+	MathProperties mathProperties() {
+		return new MathProperties();
+	}
+
+	@Bean
+	SpecialSelector specialSelector(MathProperties math) {
+		return new BestSpecialSelector(math);
+	}
+}
+
+@Component
+@AllArgsConstructor
+class TestDataInit implements ApplicationRunner {
+
+	private final SpecialRepository specials;
+
+	@Override
+	public void run(ApplicationArguments args) {
+		if (specials.findAll().size() == 0) {
+			specials.save(new Special(null, 1, 3, BigDecimal.valueOf(70.0)));
+			specials.save(new Special(null, 2, 2, BigDecimal.valueOf(15.0)));
+			specials.save(new Special(null, 3, 4, BigDecimal.valueOf(60.0)));
+			specials.save(new Special(null, 4, 2, BigDecimal.valueOf(40.0)));
+		}
 	}
 }
