c70e03282e6f0ee78a4853ce9d9fa440764eb314
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-22 10:20:34 +0200

    Enabled health details. Added custom health indicator.

--- warehouse/src/main/java/io/spring/lab/warehouse/WarehouseApplication.java
+++ warehouse/src/main/java/io/spring/lab/warehouse/WarehouseApplication.java
@@ -1,12 +1,31 @@
 package io.spring.lab.warehouse;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class WarehouseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WarehouseApplication.class, args);
+	}
+}
+
+@Component
+class RandomHealthIndicator implements HealthIndicator {
+
+	private static final Random RANDOM = new Random();
+
+	@Override
+	public Health health() {
+		return Health.status(RANDOM.nextBoolean() ? Status.UP : Status.DOWN)
+				.withDetail("someNumber", RANDOM.nextLong())
+				.build();
 	}
 }
