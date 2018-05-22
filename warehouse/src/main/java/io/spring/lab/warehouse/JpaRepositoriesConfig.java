12eb59329f99f5f1e9b7ba1aebabc86fa1357a95
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-22 10:09:56 +0200

    Fixed failing test

--- warehouse/src/main/java/io/spring/lab/warehouse/JpaRepositoriesConfig.java
+++ warehouse/src/main/java/io/spring/lab/warehouse/JpaRepositoriesConfig.java
@@ -1,9 +1,28 @@
 package io.spring.lab.warehouse;
 
+import java.math.BigDecimal;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.spring.lab.warehouse.item.Item;
+import io.spring.lab.warehouse.item.ItemRepository;
 
 @Configuration
 @EnableJpaRepositories(considerNestedRepositories = true)
 public class JpaRepositoriesConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "test.data", havingValue = "true", matchIfMissing = true)
+    ApplicationRunner initData(ItemRepository items) {
+        return args -> {
+            items.save(new Item(null, "A", 100, BigDecimal.valueOf(40.0)));
+            items.save(new Item(null, "B", 100, BigDecimal.valueOf(10.0)));
+            items.save(new Item(null, "C", 100, BigDecimal.valueOf(30.0)));
+            items.save(new Item(null, "D", 100, BigDecimal.valueOf(25.0)));
+        };
+    }
 }
