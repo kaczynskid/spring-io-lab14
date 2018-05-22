a0c179aaf326676083793e2b758cbf55c9c54184
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-22 08:53:22 +0200

    Added finding item by name custom query with QueryDSL

--- warehouse/src/main/java/io/spring/lab/warehouse/item/JpaItemRepository.java
+++ warehouse/src/main/java/io/spring/lab/warehouse/item/JpaItemRepository.java
@@ -1,5 +1,7 @@
 package io.spring.lab.warehouse.item;
 
+import static io.spring.lab.warehouse.item.QItem.item;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -7,16 +9,19 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.AllArgsConstructor;
 
 @Repository
 @AllArgsConstructor
 public class JpaItemRepository implements ItemRepository {
 
-    interface SpringDataItemRepository extends JpaRepository<Item, Long> {
+    interface SpringDataItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
         Item findTopByOrderByPriceDesc();
 
@@ -26,6 +31,8 @@ public class JpaItemRepository implements ItemRepository {
         @Query("from Item where name like :prefix%")
         List<Item> findByNamePrefix(@Param("prefix") String namePrefix);
 
+        @Override
+        List<Item> findAll(Predicate predicate);
     }
 
     private final SpringDataItemRepository repository;
@@ -56,6 +63,12 @@ public class JpaItemRepository implements ItemRepository {
 
     @Override
     public List<Item> findByNamePrefix(String prefix) {
-        return repository.findByNamePrefix(prefix);
+//        return repository.findByNamePrefix(prefix);
+        
+        return repository.findAll(withNameStartingWith(prefix));
+    }
+
+    private BooleanExpression withNameStartingWith(String prefix) {
+        return item.name.startsWith(prefix);
     }
 }
