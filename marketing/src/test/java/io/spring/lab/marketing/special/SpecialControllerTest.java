9ceff8ccbd22831f31980b96fa3197057aa87213
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-22 16:16:11 +0200

    Switched to registry-first approach

--- marketing/src/test/java/io/spring/lab/marketing/special/SpecialControllerTest.java
+++ marketing/src/test/java/io/spring/lab/marketing/special/SpecialControllerTest.java
@@ -15,11 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SpecialController.class)
+@ActiveProfiles("test")
 public class SpecialControllerTest {
 
     @MockBean
