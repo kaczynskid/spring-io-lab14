ec9d2e7b53d66a74fe9a623cdd2180daebf68fc2
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-23 14:42:32 +0200

    Changed turbine path

--- dashboard/src/main/java/io/spring/lab/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/io/spring/lab/dashboard/DashboardApplication.java
@@ -2,8 +2,13 @@ package io.spring.lab.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.turbine.streaming.servlet.TurbineStreamServlet;
 
 @SpringBootApplication
 @EnableHystrixDashboard
@@ -13,4 +18,11 @@ public class DashboardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
 	}
+
+	// org.springframework.cloud.netflix.turbine.TurbineHttpConfiguration
+	@Bean
+	public ServletRegistrationBean turbineStreamServlet() {
+		return new ServletRegistrationBean(new TurbineStreamServlet(), "/admin/turbine.stream");
+	}
+
 }
