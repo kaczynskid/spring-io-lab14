dc89aef01fdb478c95011ca6a291a83f5c661474
Darek Kaczyński <kaczynskid@gmail.com>
2018-05-23 12:25:23 +0200

    Added store special calculation

--- marketing/src/main/java/io/spring/lab/marketing/special/SpecialController.java
+++ marketing/src/main/java/io/spring/lab/marketing/special/SpecialController.java
@@ -5,11 +5,17 @@ import static java.util.stream.Collectors.toList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.spring.lab.marketing.special.calculate.SpecialCalculation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/specials")
@@ -23,4 +29,13 @@ public class SpecialController {
                 .map(SpecialRepresentation::of)
                 .collect(toList());
     }
+
+    @PostMapping("/{itemId}/calculate")
+    public SpecialCalculation calculateFor(@PathVariable("itemId") long itemId,
+                                           @RequestBody SpecialCalculationRequest request) {
+        SpecialCalculation calculation = specials.calculateFor(itemId, request.getUnitPrice(), request.getUnitCount());
+        log.info("Calculated special {}", calculation);
+        return calculation;
+    }
+
 }
